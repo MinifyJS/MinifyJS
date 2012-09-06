e6eed7a4ba590e8630346925f2d55135c1d3e56e
Richard van Velzen <rvanvelzen@expert-shops.com>
2012-09-06 14:57:05 +0200

    Improve the form a bit

--- index.php
+++ index.php
@@ -8,7 +8,33 @@ ini_set('display_errors', 'on');
 error_reporting(E_ALL | E_STRICT);
 
 if (isset($_POST['code'])) {
-	$process = proc_open(__DIR__ . '/min.php', array(
+	$options = '';
+	if (isset($_POST['options']) && is_array($_POST['options'])) {
+		foreach(array_keys($_POST['options']) as $option) {
+			switch ($option) {
+			case 'ncb':
+				$options .= ' --no-crush-bool';
+				break;
+			case 'us':
+				$options .= ' --unsafe';
+				break;
+			case 'nm':
+				$options .= ' --no-mangle';
+				break;
+			case 'b':
+				$options .= ' --beautify';
+				break;
+			case 'sd':
+				$options .= ' --strip-debug';
+				break;
+			case 'tl':
+				$options .= ' --toplevel';
+				break;
+			}
+		}
+	}
+
+	$process = proc_open(__DIR__ . '/min.php' . $options, array(
 	   0 => array('pipe', 'r'),
 	   1 => array('pipe', 'w'),
 	), $pipes, __DIR__, array(
@@ -36,11 +62,29 @@ if (isset($_POST['code'])) {
 echo '<!DOCTYPE html>
 <meta charset="UTF-8">
 <title>MinifyJS</title>
-<style>body{width:980px;margin:25px auto 0;font:16px Georgia,serif}textarea{width:100%;height:175px;font:13px monospace}input{margin:25px 0}</style>
-<h1>MinifyJS</h1>
+<style>body{width:980px;margin:25px auto 0;font:16px Georgia,serif}textarea{width:100%;height:175px;font:13px monospace}input,p{margin:5px 0;}input[type=submit]{margin:25px 0}</style>
+<h1><a href="https://github.com/MinifyJS/MinifyJS">MinifyJS</a></h1>
 <form action="" method="post">
-	<textarea name="code">' . (isset($_POST['code']) ? htmlspecialchars($_POST['code']) : '') . '</textarea>
-	<input type="submit" value="Minify!">
+ <textarea name="code">' . (isset($_POST['code']) ? htmlspecialchars($_POST['code']) : '') . '</textarea>
+ <p>';
+foreach(array(array(
+	'ncb' => "Don't crush booleans",
+	'us' => 'Unsafe transformations',
+	'nm' => 'No mangling'
+), array(
+	'b' => 'Beautify',
+	'sd' => 'Strip debug expressions',
+	'tl' => 'Mangle toplevel variables'
+)) as $options) {
+	foreach($options as $option => $name) {
+		echo '
+  <input type="checkbox" name="options[' . $option . ']" value="1"' . (isset($_POST['options'][$option]) ? ' checked' : '') . ' id="' . $option . '"> <label for="' . $option . '">' . $name . '</label>';
+	}
+	echo '<br>';
+}
+echo '
+ </p>
+ <input type="submit" value="Minify!">
 </form>';
 
 if (isset($output)) {
